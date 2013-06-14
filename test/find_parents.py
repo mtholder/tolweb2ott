71f197eca68bfc92fb3f56ce3bbd0eaa69fe2578
robinmei <robinmei@ucla.edu>
2013-06-14 13:23:43 -0500

    Gets all cdata from xml

---
+++ test/find_parents.py
@@ -0,0 +1,25 @@
+import xml.etree.ElementTree as ET
+import xml.dom as dom
+#can't import Node.parentNode
+
+tx_file = open('primates_taxonomy.txt')
+syn_file = open('primates_synonyms.txt')
+xml = ET.parse('primates_tolweb.xml')
+root = xml.getroot()
+print root
+
+def find_parents():
+    for child in root.iter():
+        n = str(child.text)
+        a = str(child.attrib)
+        x = str(child.tag)
+#         print n, '\n\n'
+        if x == 'NAME' and n != 'None': # and parent == ??
+             list.append(n)
+             print n
+#            get parent node
+
+
+list = []
+find_parents()
+print list
