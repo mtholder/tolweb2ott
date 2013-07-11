d28a50119cc21556800bc386c94e87616be2b867
robinmei <robinmei@ucla.edu>
2013-07-11 15:57:07 -0500

    node class

--- test/node_class.py
+++ test/node_class.py
@@ -2,6 +2,25 @@
 #run from tolweb2ott/test
 #RMJ
 
+#trying to figure out how to make 
+#1) dictionary with counts of homonyms
+#2) class for nodes
+# somthing like
+# class Node(object):
+#     def __init__(self, name):
+#         self.children = []
+#         self.parent = None
+#         self.name = name
+#         self.leaf_set = set()
+#     def add_child(self, node):
+#         self.children.append(node)
+#         assert node.parent is None
+#         node.parent = self
+# name2node = {}
+#right now deleting tnl and xnl since it only flags homonyms and not their count
+
+
+
 #modules
 import xml.etree.ElementTree as ET
 
@@ -16,14 +35,16 @@ def populate_xmldicts():
         otext = None
         if name is not None:
             name = name.strip()
-            if name in xnl:
-                i = xnl.index(name)
-                xnl[i] = [xnl[i], name]
+            if name in xhmns:
+                xhmns[name] += 1
             else:
-                xnl.append(name)
+            	xhmns[name] = 1
             for on_element in n_element.findall('OTHERNAMES'):
                 oname_el = n_element.find('.//OTHERNAME/NAME')
                 otext = oname_el.text
+                oname = otext.strip()
+
+
 
         xml_dict[n_id] = [n_id, n_par, name, otext]
         
@@ -31,10 +52,6 @@ def populate_xmldicts():
             xml_syn = [name, otext]
             xml_syn_dict[n_id] = xml_syn
         xml_dict[n_id] = [n_id, n_par, name]
-
-    for item in xnl:
-         if type(item) == list:
-                print 'xml\t', item
 
 
 def populate_taxdicts():
@@ -45,23 +62,20 @@ def populate_taxdicts():
         taxon = data[:3]
         name = taxon[2].strip()
         if name != 'name':
-            if name in tnl:
-                i = tnl.index(name)
-                tnl[i] = [tnl[i], name]
+            if name in thmns:
+                thmns[name] += 1
             else:
-                tnl.append(name)
+            	thmns[name] = 1
             tax_dict[taxon[0]] = taxon
 
     #synfile is organized by otol ID so don't worry about homonyms here? 
     for synline in syn_file:
         syndata = (synline.split('\t|\t'))
-        for key in tax_dict.iteritems():
-            if syndata[1] == key[0]:
-                tax_syn_dict[syndata[1]] = [key[0], syndata[0]]
+    for key in tax_dict.iteritems():
+    	sname = syndata[0].strip()
+        if syndata[1] == key[0]:
+            tax_syn_dict[syndata[1]] = [key[0], sname]
 
-    for item in tnl:
-        if type(item)== list:
-            print 'tax\t', item
 
 def combine_dicts():
     for tid in tax_dict.iteritems():
@@ -71,9 +85,7 @@ def combine_dicts():
             if tname == xname:
                 matches_dict[tid[0]] = tid[1][:2] + xid[1]
 
-
 def find_missing():
-
     for xname in xml_dict.iteritems():
         if xname[1][2] != None:
             xlist.append(xname[1][2])
@@ -100,15 +112,26 @@ xml_syn_dict = {}
 tlist = []
 xlist = []
 namelist = []
-xnl = []
-tnl = []
-
+thmns = {}
+xhmns = {}
 
 #let's go
 populate_taxdicts()
 populate_xmldicts()
 combine_dicts()
 find_missing()
+
+for key in xhmns.iteritems():
+	if key[1] > 1:
+		print key
+
+
+
+for key in thmns.iteritems():
+	if key[1] > 1:
+		print key
+
+
 
 #cleanup
 tax_file.close()
